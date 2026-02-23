4e5cc95da5b6a7a8e9aa205eefbccf62036ad94d
Kalizra <kalkidanisrael6@gmail.com>
2026-02-23 20:48:49 +0300

    cleared some unessar info

--- src/component/pages/Contact/Contact.jsx
+++ src/component/pages/Contact/Contact.jsx
@@ -5,9 +5,10 @@ function Contact() {
   return (
     <section id="contact" className="contact-section">
       <h1>Contact Me</h1>
-      <p>Email: kalkidan@example.com</p>
+      <p>Email: kalkidanisrael6@gmail.com</p>
       <p>LinkedIn: <a href="https://linkedin.com/in/kalkidan-israel" target="_blank" rel="noopener noreferrer">linkedin.com/in/kalkidan-israel</a></p>
       <p>GitHub: <a href="https://github.com/kalkidan-israel" target="_blank" rel="noopener noreferrer">github.com/kalkidan-israel</a></p>
+     
     </section>
   );
 }
